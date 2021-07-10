e9b767485d15de8634ef9b5483a16fdf8d710a84
Sohail Haqyar <haqyarsohail@gmail.com>
2021-07-11 02:50:36 +0300

    fixed oauthloading and homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,19 @@
-import { usePosts } from "../context/PostsContext";
+import { useAllPostsQuery } from "../generated/graphql";
 import { Container } from "./Container";
 import { CreatePostForm } from "./CreatePostForm";
 import PostFeed from "./PostFeed";
 import { SkeletonPost } from "./Skeletons/SkeletonPost";
 
 export const Home = () => {
-  const { data, loading, refetch } = usePosts();
-
-  console.log(data);
+  const { data, loading, refetch, error } = useAllPostsQuery();
+  if (error) console.error(error);
   return (
     <Container>
-      <CreatePostForm refetch={refetch as any} />
+      <CreatePostForm refetch={refetch} />
       {loading ? (
         [1, 2, 3, 4, 5].map((e) => <SkeletonPost key={e} />)
       ) : (
-        <PostFeed data={data} />
+        <PostFeed data={data?.findAll as any} />
       )}
     </Container>
   );
