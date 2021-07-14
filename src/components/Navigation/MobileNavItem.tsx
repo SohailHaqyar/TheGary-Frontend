cd2b9bdbb2933abe56b7283b775e907baed60ce7
Sohail Haqyar <haqyarsohail@gmail.com>
2021-07-14 13:34:54 +0300

    fixed login doing nothing

--- src/components/Navigation/MobileNavItem.tsx
+++ src/components/Navigation/MobileNavItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import { classNames } from "../SideNavigation/classNames";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 import { NavigationIcons } from "../SideNavigation/NavIcon";
 import { MobileNavIcon } from "./MobileNavIcon";
 
@@ -14,17 +14,28 @@ export const MobileNavItem: React.FC<Props> = ({
   icon,
   title,
 }) => {
-  const location = useLocation();
-  let current = location.pathname;
+  const history = useHistory();
+  const { setCurrentUser } = useAuth();
+  const handleClick = () => {
+    if (url !== "/logout") {
+      history.push(url);
+    } else {
+      if (window.confirm("Are you sure you want to logout?")) {
+        setCurrentUser(null);
+        localStorage.removeItem("token");
+        history.push("/signup");
+      }
+    }
+  };
   return (
     <div>
-      <Link
-        to={url}
+      <button
+        onClick={handleClick}
         className="w-full block py-5 px-2 text-center dark:text-gray-200 text-black-900 transition duration-300"
       >
         <MobileNavIcon url={url} iconType={icon} />
         {title}
-      </Link>
+      </button>
     </div>
   );
 };
